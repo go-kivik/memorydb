5fee5619964d051ffa421c10474a4fdcfa5f940b
Jonathan Hall <flimzy@flimzy.com>
2017-08-19 17:27:49 +0200

    Don't use Docker in tests

--- travis/install.sh
+++ travis/install.sh
@@ -27,32 +27,8 @@ function wait_for_server {
     printf "ready!\n"
 }
 
-function setup_couch16 {
-    if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-        return
-    fi
-    docker pull couchdb:1.6.1
-    docker run -d -p 6000:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=abc123 --name couchdb16 couchdb:1.6.1
-    wait_for_server http://localhost:6000/
-    curl --silent --fail -o /dev/null -X PUT http://admin:abc123@localhost:6000/_config/replicator/connection_timeout -d '"5000"'
-}
-
-function setup_couch20 {
-    if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-        return
-    fi
-    docker pull klaemo/couchdb:latest
-    docker run -d -p 6001:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=abc123 --name couchdb20 klaemo/couchdb:2.0.0
-    wait_for_server http://localhost:6001/
-    curl --silent --fail -o /dev/null -X PUT http://admin:abc123@localhost:6001/_users
-    curl --silent --fail -o /dev/null -X PUT http://admin:abc123@localhost:6001/_replicator
-    curl --silent --fail -o /dev/null -X PUT http://admin:abc123@localhost:6001/_global_changes
-}
-
 case "$1" in
     "standard")
-        setup_couch16
-        setup_couch20
         generate
     ;;
     "gopherjs")
@@ -82,7 +58,6 @@ case "$1" in
         )
 
         go get -u -d -tags=js github.com/gopherjs/jsbuiltin
-        setup_couch20
         generate
     ;;
     "linter")
