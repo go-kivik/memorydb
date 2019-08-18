e714d4f822fa8323b5237ec208d42babf4349297
Jonathan Hall <flimzy@flimzy.com>
2019-08-18 16:04:19 +0200

    Switch to new linter

--- travis/test.sh
+++ travis/test.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 set -euC
 
-function join_list {
-    local IFS=","
-    echo "$*"
-}
+go test -race ./...
 
-go test -race $(go list ./... | grep -v /vendor/)
-
-# Only run GopherJS tests,  Linter and coveragetests on Linux/Go 1.12
+# Only run GopherJS tests, linter and coverage tests on Linux/Go 1.12
 if [[ ${TRAVIS_OS_NAME} == "linux" && "${TRAVIS_GO_VERSION}" == "1.12.x" ]]; then
-    gopherjs test $(go list ./... | grep -v /vendor/)
+    gopherjs test ./...
 
     # Linter
-    diff -u <(echo -n) <(gofmt -e -d $(find . -type f -name '*.go' -not -path "./vendor/*"))
-    go install # to make gotype (run by gometalinter) happy
-    gometalinter.v1 --config .linter_test.json
-    gometalinter.v1 --config .linter.json
+    golangci-lint run ./...
 
     # Coverage
     go test -coverprofile=coverage.txt -covermode=set && bash <(curl -s https://codecov.io/bash)
